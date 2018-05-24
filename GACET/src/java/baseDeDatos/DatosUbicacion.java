dc84b4bec3eeb1962bcdbf1b7dff10d09259e4c4
sebaschb <38675783+sebaschb@users.noreply.github.com>
2018-05-23 21:46:25 -0500

    Entrega de bases de datos
    Revisar el guardado del gregorialcalendar en la base de datos, puede tener errores.

---
+++ GACET/src/java/baseDeDatos/DatosUbicacion.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseDeDatos;
+
+import Logica.Ubicacion.Ubicacion;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sebas
+ */
+public class DatosUbicacion {
+    
+    Conexion c;
+
+    public DatosUbicacion(String nombreBase , String login , String pasword) {
+        c = new Conexion(nombreBase, login, pasword);
+        
+    }
+    public void IngresarUbicacion(Ubicacion u){
+        
+        try {
+            PreparedStatement insertar = c.getConexion().prepareStatement("insert into Ubicacion("
+                    + "Longitud,"
+                    + "Latitud) values(?,?)");
+            
+            insertar.setDouble(1, u.getLatitud());
+            insertar.setDouble(2, u.getLongitud());
+            insertar.executeUpdate();
+            
+            
+        } catch (SQLException e) {
+            
+            System.out.println("ERROR : No se logro ingresar los datos a Ubicacion");
+        }
+        
+        
+    }
+    
+    
+    public void borrarUbicacion(int id) {
+
+        try {
+            PreparedStatement pstm = c.getConexion().prepareStatement("delete from Ubicacion "
+                    + " where id_Ubicacion = ?");
+
+            pstm.setInt(1, id);
+
+            pstm.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error: No se ha logrado borrar la ubicacion");
+        }
+
+
+    }
+    
+
+}
